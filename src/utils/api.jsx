4fd03a5f44eb6de8ee5432d80ed0be87f727d903
Chuck Wongus <charles.t.wongus@gmail.com>
2017-06-27 21:17:08 -0400

    First Commit

---
+++ src/utils/api.jsx
@@ -0,0 +1,16 @@
+var Fetch = require('whatwg-fetch');
+var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiUrl = '&appid=ef4d68b162ebdd76e5ca24f9aa73b79e';
+
+module.exports = {
+    get: function(place) {
+        return fetch(rootUrl + place + apiUrl, {
+            headers: {
+                // No need for special headers
+            }
+        })
+        .then(function(response) {
+            return response.json();
+        });
+    }
+};
